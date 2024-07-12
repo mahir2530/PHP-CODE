781c93ee868b052fef9410566051972bb398d47e
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2024-07-12 18:26:22 +0530

    pattern(Star).php

--- pattern(Star).php
+++ pattern(Star).php
@@ -1,11 +1,11 @@
 <html>
 <body bgcolor="#92908D">
-	<h1>This is Reversed star pattern</h1>
+	<h1>This is star pattern</h1>
 	<p style="color: #000000;">
 		<?php
 		for($i=1; $i<=5; $i++)
 		{
-			for($j=5; $j>=$i; $j--)
+			for($j=1; $j<=$i; $j++)
 			{
 				echo " *";
 			}
