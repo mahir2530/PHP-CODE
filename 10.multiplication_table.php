240f997a56b5273812ff265432dc6882344a437c
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2024-07-21 17:05:32 +0530

    10.multiplication_table.php

--- 10.multiplication_table.php
+++ 10.multiplication_table.php
@@ -1,5 +1,31 @@
-<?php
-    for ($i = 1; $i <= 10; $i++) {
-        echo "5 * $i = " . (5 * $i) . "<br>";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication Table</title>
+</head>
+
+<body>
+    <form method="POST">
+        Enter a number: <input type="text" name="number" required>
+        <input type="submit" name="submit" value="Generate Table">
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        $n = $_POST['number'];
+
+        if (is_numeric($n)) {
+            for ($i = 1; $i <= 10; $i++) {
+                echo "$n * $i = " . ($n * $i) . "<br>";
+            }
+        } else {
+            echo "Please enter a valid number.";
+        }
     }
-?>
+    ?>
+</body>
+
+</html>
