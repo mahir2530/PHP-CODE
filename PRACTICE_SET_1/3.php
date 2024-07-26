045405e29496c4ab0b4f2adb16ae0d2bb7648e91
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2024-07-26 21:59:18 +0530

    3.php

--- PRACTICE_SET_1/3.php
+++ PRACTICE_SET_1/3.php
@@ -1,10 +1,5 @@
 <?php
 $number = 12345;
-$count = 0;
-
-while ($number != 0) {
-    $number = (int)($number / 10);
-    $count++;
-}
+$count = strlen((string)$number);
 
 echo "Number of digits in $number is: $count";
