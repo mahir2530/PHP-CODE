fe5043f74792ebdeba9b3c883e96b1f93b4fe7b6
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2024-07-26 10:49:49 +0530

    5.php

--- PRACTICE_SET_4/5.php
+++ PRACTICE_SET_4/5.php
@@ -1,33 +1,10 @@
+<!-- Find The Duplicate Email -->
 <?php
-$emails = [
-    'user1@example.com',
-    'user2@example.com',
-    'user1@example.com',
-];
-
-// Initialize arrays to store email counts and non-unique emails
-$emailCounts = [];
-$nonUniqueEmails = [];
-
-// Count occurrences of each email address
-foreach ($emails as $email) {
-    if ($emailCounts[$email]) {
-        $emailCounts[$email]++;
-    } else {
-        $emailCounts[$email] = 1;
+    $arr=array("tirth@gmail.com",'Dhruv@gmail.com','tirth@gmail.com');
+    foreach($arr as $key=>$val){
+        unset($arr[$key]);
+        if(in_array($val,$arr)){
+            echo "The duplicate Email is : ".$val." ";
+        }
     }
-}
-
-// Identify email addresses that appear more than once
-foreach ($emailCounts as $email => $count) {
-    if ($count > 1) {
-        $nonUniqueEmails[] = $email;
-    }
-}
-
-// Print non-unique email addresses
-echo "Non-unique email addresses:\n";
-foreach ($nonUniqueEmails as $email) {
-    echo "$email\n";
-}
 ?>
