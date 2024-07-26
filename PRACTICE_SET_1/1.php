a9df454486d7b45bc07c462e90476a4e5cd34d16
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2024-07-26 21:51:59 +0530

    1.php

--- PRACTICE_SET_1/1.php
+++ PRACTICE_SET_1/1.php
@@ -3,7 +3,7 @@ $character = 'a'; // Replace with the character you want to check
 
 $vowels = ['a', 'e', 'i', 'o', 'u'];
 
-if (in_array(strtolower($character), $vowels)) {
+if (in_array($character), $vowels) {
     echo "$character is a vowel.";
 } else {
     echo "$character is a consonant.";
