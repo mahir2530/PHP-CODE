c4b5bd9999b9e4c2f87680ef87e2e989975cd8fc
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2024-08-05 16:46:50 +0530

    39.TyprOfSort.php

--- 39.TyprOfSort.php
+++ 39.TyprOfSort.php
@@ -5,6 +5,14 @@ $n = array('cow' => 1, 'lion' => 5, 'tiger' => 3, 'dog' => 2);
 echo "Original array:\n";
 print_r($n);
 
+echo "\nNormal sort:\n";
+sort($n);
+print_r($n);
+
+echo "\nReverseNormal sort:\n";
+rsort($n);
+print_r($n);
+
 echo "\nAsort (associative sort):\n";
 asort($n);
 print_r($n);
