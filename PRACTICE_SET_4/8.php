0e471205f4eb62c64eb65dd59285ae54075f3c97
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2024-08-06 12:52:38 +0530

    8.php

--- PRACTICE_SET_4/8.php
+++ PRACTICE_SET_4/8.php
@@ -1,7 +1,8 @@
 <?php
 
 $data = array("Sophia" => 31, "Jacob" => 41, "William" => 39, "Ramesh" => 40);
-$highestValue = max($data);
+$key=array_keys($data);
+$highestValue = max($key);
 
 
 echo "The index (key) of the highest value is: $highestValue\n";
