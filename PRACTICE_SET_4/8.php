1f38a4e5f4e9dafeab0be6bead09f3507de8c597
Tirth Vaghela <115704585+Tirthvaghela@users.noreply.github.com>
2024-08-06 13:30:09 +0530

    8.php

--- PRACTICE_SET_4/8.php
+++ PRACTICE_SET_4/8.php
@@ -1,10 +1,19 @@
 <?php
 
-$data = array("Sophia" => 31, "Jacob" => 41, "William" => 39, "Ramesh" => 40);
-$key=array_keys($data);
-$highestValue = max($key);
+$array_movie = array(1=>45,2=>34,3=>90,4=>60);
+
+$high_index = array_keys($array_movie);
+$high_values = current($array_movie);
+
+foreach ($array_movie as $key => $value) {
+	
+	if ($value > $high_values) {
+		
+		$high_values = $value;
+		$high_index = $key;
+	}
+}
 
 
-echo "The index (key) of the highest value is: $highestValue\n";
-
+echo $high_values . " is having highest index of ". $high_index;
 ?>
