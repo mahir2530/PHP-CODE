c1498eeb1a4aa8ae76a317d3572c6654b59e839b
Mahir Patel <patelmahir820@gmail.com>
2024-10-31 09:50:30 +0530

    Create 52.session.php

---
+++ 52.session.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SESSION</title>
+</head>
+<body>
+        <?php
+            $_SESSION['favcolor']='red';
+            $_SESSION['favcolor']='red';
+            echo"Session are set";
+        ?>
+</body>
+</html>
